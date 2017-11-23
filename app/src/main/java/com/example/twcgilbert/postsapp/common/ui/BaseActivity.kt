a777533739109ae220bf01f35d51abeba2429f91
Tom Gilbert <tom@alteredworlds.com>
2017-11-23 21:45:14 +0000

    BaseAactivity abstract rather than open => no warning re manifest

--- app/src/main/java/com/example/twcgilbert/postsapp/common/ui/BaseActivity.kt
+++ app/src/main/java/com/example/twcgilbert/postsapp/common/ui/BaseActivity.kt
@@ -6,7 +6,7 @@ import dagger.android.support.DaggerAppCompatActivity
 /**
  * Created by twcgilbert on 01/10/2017.
  */
-open class BaseActivity : DaggerAppCompatActivity(), BaseContract.View {
+abstract class BaseActivity : DaggerAppCompatActivity(), BaseContract.View {
 
     override fun showError(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
