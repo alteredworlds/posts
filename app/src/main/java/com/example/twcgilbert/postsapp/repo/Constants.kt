e20162708ae18e8a2b964ee012d92f0ffea8cec0
Tom Gilbert <tom@alteredworlds.com>
2018-04-13 15:41:29 +0100

    Kotlin const works for Strings...

--- app/src/main/java/com/example/twcgilbert/postsapp/repo/Constants.kt
+++ app/src/main/java/com/example/twcgilbert/postsapp/repo/Constants.kt
@@ -5,9 +5,9 @@ package com.example.twcgilbert.postsapp.repo
  */
 object Constants {
 
-    @JvmField val BASE_URL = "http://jsonplaceholder.typicode.com/"
+    const val BASE_URL = "http://jsonplaceholder.typicode.com/"
 
-    @JvmField val ADORABLE_URL = "https://api.adorable.io/avatars/128/"
+    const val ADORABLE_URL = "https://api.adorable.io/avatars/128/"
 
-    @JvmField val IMAGE_EXTENSION = ".png"
+    const val IMAGE_EXTENSION = ".png"
 }
