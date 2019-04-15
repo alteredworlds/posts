1549409ee18eae7828e401c87b39f8bbea251a81
Tom Gilbert <tom@alteredworlds.com>
2019-04-15 15:30:49 +0100

    Workaround in Java due to Dagger/Kotlin not playing well together as of now see: https://github.com/google/dagger/issues/1478

--- app/src/main/java/com/example/twcgilbert/postsapp/common/di/ViewModelFactory.kt
+++ app/src/main/java/com/example/twcgilbert/postsapp/common/di/ViewModelFactory.kt
@@ -2,11 +2,9 @@ package com.example.twcgilbert.postsapp.common.di
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import dagger.MapKey
 import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
-import kotlin.reflect.KClass
 
 @Singleton
 class ViewModelFactory @Inject constructor(
@@ -17,8 +15,3 @@ class ViewModelFactory @Inject constructor(
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = viewModels[modelClass]?.get() as T
 }
-
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-@Retention(AnnotationRetention.RUNTIME)
-@MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
